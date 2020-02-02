70d585b0a7ed368c8cd16c7cfa5a3ce10d53a506
Carlos Ruiz <karlosarr@gmail.com>
2020-02-02 12:05:48 -0600

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://carlosruiz2.esy.es/';
+$config['base_url'] = 'https://lavendimia2.herokuapp.com/';
 //$config['base_url'] = 'http://127.0.0.1/lavendimia/';
 /*
 |--------------------------------------------------------------------------
